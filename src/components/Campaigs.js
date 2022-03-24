ac58276f40d7af7e6bdbff0845160841d955f77b
YUNUS EMRE MERMER <yunusemremermer57@gmail.com>
2022-03-24 11:48:15 +0300

    24.03.22

--- src/components/Campaigs.js
+++ src/components/Campaigs.js
@@ -17,12 +17,35 @@ useEffect(() => {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     speed: 500,
     autoplaySpeed: 3500,
-    cssEase: "linear"
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          
+        }
+      }
+    ]
 
   };
 
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,8 +1,37 @@
 import React from 'react'
 
 function Cards() {
+
+  const cards = [
+    {
+			title: 'Her siparişinize bir kampanya',
+			description: 'Getir\'de vereceğiniz her siparişe uygun bir kampanya bulabilirsiniz.',
+			image: 'https://getir.com/_next/static/images/intro-in-minutes-a7a9238a73013642a6597c4db06653c1.svg'
+		},
+		{
+			title: 'Dakikalar içinde kapınızda',
+			description: 'Getir’le siparişiniz dakikalar içinde kapınıza gelir.',
+			image: 'https://getir.com/_next/static/images/intro-market-courier-34cd8b0ca1d547580d506566edfacf8d.svg'
+		},
+		{
+			title: 'Binlerce çeşit mutluluk',
+			description: 'Getir’de binlerce çeşit arasından seçiminizi yapabilirsiniz.',
+			image: 'https://getir.com/_next/static/images/intro-discount-6248544cb695830a2e25debd3c0f3d29.svg'
+		}
+  ]
+
   return (
-    <div>Cards</div>
+    <div className='mt-6 grid grid-cols-3 gap-x-4 '>
+      {
+          cards.length && cards.map( card => (
+            <div className='bg-white p-10 rounded-lg shadow-md flex flex-col items-center text-center'>
+              <img className='w-[150px] h-[150px] mb-6' src={card.image} />
+              <h6 className='font-semibold text-lg text-primary-brand-color'>{card.title}</h6>
+              <p className='mt-2 text-sm text-gray-700 '>{card.description}</p>
+            </div>
+          ))
+      }
+    </div>
   )
 }
 
